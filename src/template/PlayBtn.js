39f4af3e20daea1f3b3ce09d2be467c31dd4fcf6
Dmitrii <dimaarion@gmail.com>
2025-04-03 00:21:02 +0300

    https://github.com/dimaarion/batll-tank-react.git

---
+++ src/template/PlayBtn.js
@@ -0,0 +1,19 @@
+export default function PlayBtn(){
+    return <>
+
+        <svg width="53" height="50" viewBox="0 0 53 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(1.5 0)">
+                <g fill="#1F2324" fillRule="evenodd">
+                    <rect width="49.99" height="50" />
+                    <path d="M49.99 0L0 0L0 50L49.99 50L49.99 0ZM4 46L4 4L45.99 4L45.99 46L4 46Z"  className={"tank-btn"} fillRule="evenodd" />
+                </g>
+                <g transform="translate(0 13.125)">
+                    <path d="M0 0L50 0" fill="#3C4546" fillRule="evenodd" strokeWidth="3" stroke="#1F2324" strokeLinecap="square" transform="translate(0 23.75)" />
+                    <path d="M0 0L50 0" fill="#3C4546" fillRule="evenodd" strokeWidth="3" stroke="#1F2324" strokeLinecap="square" transform="translate(0 11.875)" />
+                    <path d="M0 0L50 0" fill="#3C4546" fillRule="evenodd" strokeWidth="3" stroke="#1F2324" strokeLinecap="square" />
+                </g>
+                <path d="M23.9952 15L0 30L0 0L23.9952 15Z" fill="#CEB423" fillRule="evenodd" transform="translate(14.997 10)" />
+            </g>
+        </svg>
+    </>
+}
