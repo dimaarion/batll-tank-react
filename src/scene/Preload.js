201f3261feced18aa08db545ab9d58fc47b1bcdf
Dmitrii <dimaarion@gmail.com>
2025-04-13 00:19:31 +0300

    https://github.com/dimaarion/batll-tank-react.git

--- src/scene/Preload.js
+++ src/scene/Preload.js
@@ -1,110 +1,119 @@
 import Phaser from "phaser";
-export default class Preload extends Phaser.Scene{
-  constructor() {
-    super('Preloader');
-  }
+import axios from "axios";
+
+export default class Preload extends Phaser.Scene {
+    constructor() {
+        super('Preloader');
+    }
 
 
-  preload() {
+    preload() {
+        this.load.setCORS('anonymous');
 
-    this.load.image('tiles', './img/tiled/1.png');
-    this.load.image('tiles_2', './img/tiled/2.png');
-    this.load.tilemapTiledJSON('map', './img/tiled/location_1.json');
-    this.load.tilemapTiledJSON('map2', './img/tiled/location_2.json');
+        this.load.on('progress', function (value) {
+            //console.log(value);
 
-    this.load.image("base-player", './img/sprites/base.png');
-    this.load.image("HPBasePlayer", './img/sprites/HPBasePlayer.png');
+        });
 
-    this.load.image("base-bot", './img/sprites/base-bot.png');
+        this.load.image('tiles', 'https://game.fk-i-s.ru/script/tiles.php');
 
-    this.load.image("HP-bot", './img/sprites/HP-bot.png');
-    this.load.image("HP-player", './img/sprites/HP-player.png');
+        this.load.image('tiles_2', 'https://game.fk-i-s.ru/script/tiles_2.php');
 
-    this.load.image("Hull_01", './img/sprites/PNG/Hulls_Color_A/Hull_01.png');
-    this.load.atlas('tanks', './img/sprites/tanks.png', './img/tanks.json');
+        this.load.tilemapTiledJSON('map', 'https://game.fk-i-s.ru/script/location_1-json.php');
 
-    this.load.image("Gun_01", './img/sprites/PNG/Weapon_Color_A/Gun_01.png');
-    this.load.image("Gun_01_A", './img/sprites/PNG/Weapon_Color_A/Gun_01_A.png');
+        this.load.tilemapTiledJSON('map2', 'https://game.fk-i-s.ru/script/location_2-json.php');
 
-    this.load.image("pule", './img/sprites/pule.png');
-    this.load.image("point-move", './img/sprites/pointNone.png');
+        this.load.image("base-player", 'https://game.fk-i-s.ru/script/base.php');
 
-    this.load.image("hallway", './img/sprites/hallway2.png');
+        this.load.image("HPBasePlayer", 'https://game.fk-i-s.ru/script/HPBasePlayer.php');
 
-    this.load.image("rocket-static", './img/sprites/rocket-static.png');
+        this.load.image("base-bot", 'https://game.fk-i-s.ru/script/base-bot.php');
 
-    this.load.spritesheet('pule-blast', './img/sprites/pule-blast.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    });
-    this.load.spritesheet('pule-departure', './img/sprites/pule-departure.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    });
-    this.load.spritesheet('image-point', './img/sprites/pule-on.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    });
-    this.load.spritesheet('track', './img/sprites/trak.png', {
-      frameWidth: 105,
-      frameHeight: 123
-    });
-    this.load.spritesheet('burning', './img/sprites/burning.png', {
-      frameWidth: 100,
-      frameHeight: 100
-    });
-    this.load.spritesheet('rocket', './img/sprites/rocket.png', {
-      frameWidth: 64,
-      frameHeight: 228
-    });
+        this.load.image("HP-bot", 'https://game.fk-i-s.ru/script/HP-bot.php');
+
+        this.load.image("HP-player", 'https://game.fk-i-s.ru/script/HP-player.php');
+
+        this.load.atlas('tanks', 'https://game.fk-i-s.ru/script/tanks-img.php', 'https://game.fk-i-s.ru/script/tanks-json.php');
+
+        this.load.image("pule", 'https://game.fk-i-s.ru/script/pule.php');
+
+        this.load.image("point-move", 'https://game.fk-i-s.ru/script/pointNone.php');
+
+        this.load.image("hallway", 'https://game.fk-i-s.ru/script/hallway.php');
+
+        this.load.image("rocket-static", 'https://game.fk-i-s.ru/script/rocket-static.php');
+
+        this.load.spritesheet('pule-blast', 'https://game.fk-i-s.ru/script/pule-blast.php', {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet('pule-departure', 'https://game.fk-i-s.ru/script/pule-departure.php', {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet('image-point', 'https://game.fk-i-s.ru/script/cursor.php', {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet('track', 'https://game.fk-i-s.ru/script/trak.php', {
+            frameWidth: 105,
+            frameHeight: 123
+        });
+        this.load.spritesheet('burning', 'https://game.fk-i-s.ru/script/burning.php', {
+            frameWidth: 100,
+            frameHeight: 100
+        });
+
+        this.load.spritesheet('rocket', 'https://game.fk-i-s.ru/script/rocket.php', {
+            frameWidth: 64,
+            frameHeight: 228
+        });
 
 
+    }
 
+    create() {
+        this.scene.start('Start');
+        this.anims.create({
+            key: 'runPoint',
+            frames: this.anims.generateFrameNumbers('image-point', {start: 0, end: 3}),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'pule-blast-run',
+            frames: this.anims.generateFrameNumbers('pule-blast', {start: 0, end: 7}),
+            frameRate: 30,
+            repeat: 0
+        });
 
-  }
+        this.anims.create({
+            key: 'burning',
+            frames: this.anims.generateFrameNumbers('burning', {start: 0, end: 9}),
+            frameRate: 10,
+            repeat: -1
+        });
 
-  create(){
-    this.scene.start('Start');
-    this.anims.create({
-      key: 'runPoint',
-      frames: this.anims.generateFrameNumbers('image-point', {start: 0, end: 3}),
-      frameRate: 5,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'pule-blast-run',
-      frames: this.anims.generateFrameNumbers('pule-blast', {start: 0, end: 8}),
-      frameRate: 30,
-      repeat: 0
-    });
+        this.anims.create({
+            key: 'pule-departure-run',
+            frames: this.anims.generateFrameNumbers('pule-departure', {start: 0, end: 3}),
+            frameRate: 50,
+            repeat: 0
+        });
 
-    this.anims.create({
-      key: 'burning',
-      frames: this.anims.generateFrameNumbers('burning', {start: 0, end: 9}),
-      frameRate: 10,
-      repeat: -1
-    });
+        this.anims.create({
+            key: 'run-track',
+            frames: 'track',
+            frameRate: 5,
+            repeat: -1
+        });
 
-    this.anims.create({
-      key: 'pule-departure-run',
-      frames: this.anims.generateFrameNumbers('pule-departure', {start: 0, end: 3}),
-      frameRate: 50,
-      repeat: 0
-    });
-
-    this.anims.create({
-      key: 'run-track',
-      frames:'track',
-      frameRate: 5,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'rocket-run',
-      frames:'rocket',
-      frameRate: 5,
-      repeat: -1
-    });
-  }
+        this.anims.create({
+            key: 'rocket-run',
+            frames: 'rocket',
+            frameRate: 5,
+            repeat: -1
+        });
+    }
 
 }
